635376e24474f64b6f0cb2c4d1204f18b2a13dc4
firman <dev.aikosia@gmail.com>
2023-04-25 02:18:42 +0700

    Release 0.2.0

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { Automaton, Decorators } from "@aikosia/automaton";
+
+class BotName extends Automaton{
+    constructor(){
+        super({key:"BotName"});
+    }
+
+    /**
+     * Automaton runtime will pass data to this function depend on automaton->runParameter field at package.json
+     * @param {Object} context||page||null
+     */
+    @Decorators.delay
+    async run(page){
+        await page.goto("https://www.google.com/doodles");
+    }
+}
+
+export default BotName;
